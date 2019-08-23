1b6121378615933d47e324faebb4d73a0b33ddfc
Arturas Zuta <arturaszuta@gmail.com>
2019-08-23 23:40:37 +0000

    Fixed another typo

--- db/schema/07_ratings.sql
+++ db/schema/07_ratings.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS ratings CASCADE;
 
 CREATE TABLE ratings (
-  id PRIMARY KEY SERIAL,
+  id SERIAL KEY PRIMARY,
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
   restaurant_id VARCHAR(20) REFERENCES restaurants(id) ON DELETE CASCADE,
   rating INTEGER,
