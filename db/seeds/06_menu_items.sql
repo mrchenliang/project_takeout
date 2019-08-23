0b077406f8a91b301f44027e9c3f581e930f0eb3
Arturas Zuta <arturaszuta@gmail.com>
2019-08-23 23:45:12 +0000

    Another typo

--- db/seeds/06_menu_items.sql
+++ db/seeds/06_menu_items.sql
@@ -1,11 +1,11 @@
-INSERTO INTO menu_items (restaurant_id, category_id, name,  price, description, image_url) VALUES (1, 1, 'Chicken Shawarma Wrap', 895, 'chicken, turnip, cabbage, hot sauce', 'https://images.unsplash.com/photo-1529006557810-274b9b2fc783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80');
+INSERT INTO menu_items (restaurant_id, category_id, name,  price, description, image_url) VALUES (1, 1, 'Chicken Shawarma Wrap', 895, 'chicken, turnip, cabbage, hot sauce', 'https://images.unsplash.com/photo-1529006557810-274b9b2fc783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80');
 
-INSERTO INTO menu_items (restaurant_id, category_id, name,  price, description, image_url) VALUES (1, 3, 'French Fries', 495, 'perfectly fried and lightly seasoned', 'https://images.unsplash.com/photo-1518013431117-eb1465fa5752?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
+INSERT INTO menu_items (restaurant_id, category_id, name,  price, description, image_url) VALUES (1, 3, 'French Fries', 495, 'perfectly fried and lightly seasoned', 'https://images.unsplash.com/photo-1518013431117-eb1465fa5752?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
 
-INSERTO INTO menu_items (restaurant_id, category_id, name,  price, description, image_url) VALUES (1, 2, 'Lamb Shish Kabob', 1195, 'Grilled lamb skewers, rice, salad', 'https://images.unsplash.com/photo-1520252729650-ddced2015543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
+INSERT INTO menu_items (restaurant_id, category_id, name,  price, description, image_url) VALUES (1, 2, 'Lamb Shish Kabob', 1195, 'Grilled lamb skewers, rice, salad', 'https://images.unsplash.com/photo-1520252729650-ddced2015543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
 
 INSERTO INTO menu_items (restaurant_id, category_id, name,  price, description, image_url) VALUES (1, 1, 'Lamb Pita Wrap', 995, 'perfectly grilled lamb, mint jelly, rice', 'https://images.unsplash.com/photo-1561620141-343a829938de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1004&q=80');
 
-INSERTO INTO menu_items (restaurant_id, category_id, name,  price, image_url) VALUES (1, 4, 'Coca Cola', 395, 'https://images.unsplash.com/photo-1529006557810-274b9b2fc783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80');
+INSERT INTO menu_items (restaurant_id, category_id, name,  price, image_url) VALUES (1, 4, 'Coca Cola', 395, 'https://images.unsplash.com/photo-1529006557810-274b9b2fc783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80');
 
-INSERTO INTO menu_items (restaurant_id, category_id, name,  price, image_url) VALUES (1, 3, 'Green Salad', 695, 'https://images.unsplash.com/photo-1556386470-bcdc6a5e9b9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=672&q=80');
+INSERT INTO menu_items (restaurant_id, category_id, name,  price, image_url) VALUES (1, 3, 'Green Salad', 695, 'https://images.unsplash.com/photo-1556386470-bcdc6a5e9b9e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=672&q=80');
