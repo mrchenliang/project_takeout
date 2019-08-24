add0e84f0658543274b92dada340bc1ae5034338
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-08-24 22:08:37 +0000

    Add login post route

--- routes/users.js
+++ routes/users.js
@@ -5,37 +5,36 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const helpers = require('../lib/dbHelpers.js');
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
+  router.get("/", (req, res) => {});
 
   //login page
-  router.get("/login", (req, res) => {
-    res.render("login");
-  });
+  router.get("/login", (req, res) => {});
 
   //browse user's orders
-  router.get("/:user_id/orders", (req, res) => {
-
-  });
+  router.get("/:user_id/orders", (req, res) => {});
 
   //browse user's specfic order
-  router.get("/:user_id/orders/:order_id", (req, res) => {
+  router.get("/:user_id/orders/:order_id", (req, res) => {});
 
+  //login the user
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    helpers.getUserByEmail(db, email)
+      .then(result => {
+        const userDetails = result[0];
+        if (userDetails.password === password) {
+          res.send("Logged In");
+        } else {
+          res.send("Incorrect credentials");
+        }
+      });
   });
-
   return router;
 };
