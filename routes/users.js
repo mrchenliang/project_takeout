910379c11d6280ff55d4bb1a1c74a320bb02fddd
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-08-25 01:18:33 +0000

    Add user route to add item to cart

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,9 @@
 
 const express = require("express");
 const router = express.Router();
-const helpers = require('../lib/dbHelpers.js');
+const helpers = require("../lib/dbHelpers.js");
 
-module.exports = (db) => {
+module.exports = db => {
   router.get("/", (req, res) => {});
 
   //login page
@@ -26,15 +26,43 @@ module.exports = (db) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    helpers.getUserByEmail(db, email)
+    helpers
+      .getUserByEmail(db, email)
       .then(result => {
         const userDetails = result[0];
         if (userDetails.password === password) {
-          res.send("Logged In");
+          res.send("Logged In\n");
         } else {
-          res.send("Incorrect credentials");
+          res.send("Incorrect credentials\n");
         }
-      });
+      })
+      .catch(e =>
+        setImmediate(() => {
+          throw e;
+        })
+      );
   });
+
+  //add an item to the users order
+  router.post("/:user_id/orders", (req, res) => {
+    const user_id = req.params.user_id;
+    const menu_item_id = req.body.menu_item_id;
+    const quantity = req.body.quantity;
+    const notes = req.body.notes;
+
+    // helpers.getUsersPendingOrder(db, user_id).then(result => console.log(result[0].id));
+    // helpers.getMenuItemPrice(db, menu_item_id).then(result => console.log(result[0].price));
+
+    helpers.addToOrder(db, user_id, menu_item_id, quantity, notes)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(e =>
+        setImmediate(() => {
+          throw e;
+        })
+      );
+  });
+
   return router;
 };
