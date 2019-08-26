6c93a9765175129dad384cdc0381e4b8674bcc73
Chen Liang <c.liang@ualberta.ca>
2019-08-26 16:50:10 +0000

    developing rest clienients side

---
+++ routes/clients.js
@@ -0,0 +1,30 @@
+/*
+ * All routes for the Clients are defined here
+ * Since this file is loaded in server.js into api/restaurants,
+ *   these routes are mounted onto /restaurants
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const helpers = require('../lib/dbHelpers.js');
+
+odule.exports = (db) => {
+  //browse restaurants
+  router.get("/", (req, res) => {
+    helpers.getAllRestaurants(db).then(result => res.send(result));
+  });
+
+  //browse restaurant menu route
+  router.get("/:restaurant_id", (req, res) => {
+    helpers.getMenuItems(db, req.params.restaurant_id).then(result => res.send(result));
+  });
+
+  //browse restaurant menu route
+  router.get("/categories/:category_id", (req, res) => {
+    helpers.getCategoryNameById(db, req.params.category_id).then(result => res.send(result));
+  });
+
+  return router;
+};
+
