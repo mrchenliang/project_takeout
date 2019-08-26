14806cb46d9c7fef55c96dd4e44b43d33d672dc9
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-08-26 23:50:57 +0000

    Add add to order modal

--- public/scripts/renderMenuItems.js
+++ public/scripts/renderMenuItems.js
@@ -83,4 +83,79 @@ const renderMenuItems = function(data) {
   $("#rootContainer").append(newTemplateString);
   generateCategories(data);
   generateMenuItems(data);
+
+  // ON CLICK LISTENER AND RENDER ADD TO CART MODAL
+  $('.singleMenuItem').on('click', function() {
+    const title = $(this).children('.menuItemDescription').children('h2')[0].innerHTML;
+    const description = $(this).children('.menuItemDescription').children('p')[0].innerHTML;
+    const price = $(this).children('.menuItemDescription').children('h3')[0].innerHTML.match(/[\d]+[.][\d]+$/gm);
+    const image = $(this).children('.menuItemImage').css("background-image");
+    let quantity = 1;
+
+    const modal =
+      `<div class="ui modal">
+        <div class="header">
+          <h1>Add to Order</h1>
+        </div>
+        <div class="image content">
+          <div class="modalImage" style='background-image: ${image}'></div>
+        </div>
+        <div class="modalTitle">
+          <h2>${title}</h2>
+        </div>
+        <div class="modalDescription">
+          ${description}
+        </div>
+        <div class="modalPrice">
+          <h2>$ ${price}</h2>
+        </div>
+        <form class="ui form" id="addItemForm">
+          <table>
+            <div class="field">
+              <label>Notes (optional):</label>
+              <input type="text" name="notes" placeholder="Make it blessed.">
+            </div>
+            <div class="field">
+              <button class="positive ui button" id="addQuantity">Add</button>
+              <button class="negative ui button" id="removeQuantity">Remove</button>
+              <h2>1 x </h2><input type="text" name="quantity" readonly style="border:none"><h3 id="quantity">${quantity}</h3>
+            </div>
+            <div class="field">
+              <h3 id=totalPrice>Total: $${quantity*price}</h3>
+            </div>
+            <div class="actions">
+              <div class="ui large buttons">
+                <button class="ui button active">Keep Browsing</button>
+                <div class="or"></div>
+                <button class="ui button">Checkout</button>
+              </div>
+            </div>
+          </table>
+        </form>
+      </div>`;
+
+    $("#rootContainer").append(modal);
+
+    $('#addQuantity').on('click', function() {
+      event.preventDefault();
+      quantity++;
+      $('#quantity')[0].innerHTML = `${quantity}`;
+      $('#totalPrice')[0].innerHTML = `Total: $${(quantity*price).toFixed(2)}`;
+      let count = $("#quantity")[0].innerHTML;
+      $('#addItemForm input[name=quantity]').val(count);
+      console.log(count);
+      console.log($('#addItemForm input[name=quantity]').val());
+    });
+
+    $('#removeQuantity').on('click', function() {
+      event.preventDefault();
+      if (quantity > 1) {
+        quantity--;
+        $('#quantity')[0].innerHTML = `${quantity}`;
+        $('#totalPrice')[0].innerHTML = `Total: $${(quantity*price).toFixed(2)}`;
+      };
+    });
+
+    $('.ui.modal').modal('show');
+  });
 };
