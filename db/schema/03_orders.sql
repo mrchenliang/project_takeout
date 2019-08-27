6b640b95d6625a558172d3593557501bf351e636
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-08-27 15:03:50 +0000

    Add route to get pending order details

--- db/schema/03_orders.sql
+++ db/schema/03_orders.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  restaurant_id VARCHAR(20) REFERENCES restaurants(id) ON DELETE CASCADE,
+  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
   status VARCHAR(30) DEFAULT 'pending',
   placed_at DATE NOT NULL,
   wait_time INTEGER NOT NULL
