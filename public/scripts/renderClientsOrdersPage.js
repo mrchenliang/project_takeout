0ffe0d570971d6bb3962f57c23d90ef23e6c3ec9
Chen Liang <c.liang@ualberta.ca>
2019-08-28 16:37:39 +0000

    fixing scrolling

--- public/scripts/renderClientsOrdersPage.js
+++ public/scripts/renderClientsOrdersPage.js
@@ -7,7 +7,9 @@ const renderClientsOrdersPage = function(data) {
   data.forEach(element => {
     const newOrder = `
     <div class = "order-card-wrap" data-orderID='${element.order_id}'>
-    <div class="ui raised very padded text container segment status-${element.order_status}" data-orderidstatus=${element.order_id}'>
+    <div class="ui raised very padded text container segment status-${
+      element.order_status
+    }" data-orderidstatus=${element.order_id}'>
   <h2 class="ui header order-card-wrap" >Order ID: ${element.order_id} </h2>
   <div class='orders-usersID'>
   Customer Name: ${element.name}
@@ -22,12 +24,14 @@ const renderClientsOrdersPage = function(data) {
   Status: ${element.order_status}
   </div>
   <div class='confirm-completed' data-id='${element.order_id}'>
-  ${(element.order_status == 'submitted') ?
-  `<div class="ui mini icon input">
-  <input type="text" placeholder="Wait Time">
+  ${
+    element.order_status == "submitted"
+      ? `<div class="ui mini icon input">
+  <input type="text" data-est-time placeholder="Wait Time">
   </div>
-  <button class="ui secondary button"> Confirm </div>` :
-  `<button class="ui secondary button"> Pick Up </div>`}</button>
+  <button class="ui secondary button"> Confirm </div>`
+      : `<button class="ui secondary button"> Pick Up </div>`
+  }</button>
 </div>
 </div>
 </div>
@@ -43,20 +47,19 @@ const renderClientsOrdersPage = function(data) {
     const queryString = `/clients/2/orders/${orderId}`;
 
     $.ajax(queryString, { method: "GET" }).done(function(value) {
-      console.log(value)
+      console.log(value);
       renderOrdersDetail(value);
     });
   });
 
-  $('.confirm-completed').on('click', function() {
+  $(".confirm-completed").on("click", function() {
     const orderId = $(this).data().id;
     const queryString = `/clients/2/orders/${orderId}`;
-    $.ajax(queryString, { method: 'POST' })
-    .done((value)=> {console.log(value)})
-    // $("#clientsLeftContainer").empty();
-    // setTimeout($.ajax('/clients/2/orders', { method: 'GET' })
-    //   .done(function(value) {
-    //   renderClientsOrdersPage(value);
-    //   }), 3000);
+    $.ajax(queryString, { method: "POST" }).done(value => {
+      $("#clientsLeftContainer").empty();
+      $.ajax("/clients/2/orders", { method: "GET" }).done(function(value) {
+        renderClientsOrdersPage(value);
+      });
+    });
   });
 };
